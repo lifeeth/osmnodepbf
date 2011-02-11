5ab1f252757516ccc6e63d68a32e8541b9d316ad
Praneeth <lifeeth@gmail.com>
2011-02-12 03:45:43 +0530

    Added example script

---
+++ example.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import osmnodepbf
+
+foo = osmnodepbf.Parser("india.osm.pbf") 
+
+#tags = foo.return_tags(refresh=True) # To see what tags are available
+
+railway_stations = foo.parse({"railway":"station"})
+
+print len(railway_stations)
